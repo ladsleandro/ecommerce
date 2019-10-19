e8a0b8c4b8031aa5430ff8f868216ac15c42566f
Leandro Alves <lads.dev7@gmail.com>
2019-10-18 23:00:57 -0300

    Site - Produtos

--- admin-categories.php
+++ admin-categories.php
@@ -1,5 +1,6 @@
 <?php
 
+use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Category;
@@ -67,4 +68,3 @@ $app->get('/categories/:idcategory', function ($idcategory){
         'products' => []
     ));
 });
-
