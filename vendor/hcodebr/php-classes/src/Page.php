adb6e5c903cbe63c33fe9b44b74de96f1bb676f3
Leandro Alves <lads.dev7@gmail.com>
2019-10-24 12:13:04 -0300

    Ajuste Endereço

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -3,6 +3,7 @@
 namespace Hcode;
 
 use Rain\Tpl;
+use Hcode\Model\User;
 
 class Page {
 
@@ -31,6 +32,8 @@ class Page {
         Tpl::configure( $config );
 
         $this->tpl = new Tpl();
+
+        if (isset($_SESSION[User::SESSION])) $this->tpl->assign("user", $_SESSION[User::SESSION]);
 
         if ($this->options['data']) $this->setData($this->options['data']);
 
